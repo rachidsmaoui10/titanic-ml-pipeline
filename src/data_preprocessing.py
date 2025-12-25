350bd7e8f5928b48ce38fd34f7b91620e4958828
rachidsmaoui10 <rachid.smaoui@etu.cyu.fr>
2025-12-25 14:07:44 +0100

    Update data_preprocessing.py

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,13 +1,14 @@
 import pandas as pd
+import os
 
-URL = "https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv"
-
-def load_data():
-    return pd.read_csv(URL)
+def load_data(project_root):
+    path = os.path.join(project_root, "data", "titanic.csv")
+    return pd.read_csv(path)
 
 def clean_data(df):
-    df = df.drop(columns=["cabin"])
+    df = df.drop(columns=["Cabin"])
     df["Embarked"] = df["Embarked"].fillna(df["Embarked"].mode()[0])
     df["Age"] = df["Age"].fillna(df["Age"].median())
     df["Pclass"] = df["Pclass"].astype("category")
     return df
+
